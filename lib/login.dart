fcf15a4512b71aabbb9848a47dd49d6ee2c91c09
light-ai <yoneda.yuki785@mail.kyutech.jp>
2021-03-11 16:27:32 +0900

    First commit.

---
+++ lib/login.dart
@@ -0,0 +1,235 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/main.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+
+// ログイン画面用Widget
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage> {
+  // メッセージ表示用
+  String infoText = '';
+  // 入力したメールアドレス・パスワード
+  String email = '';
+  String password = '';
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleCurrentUser = _googleSignIn.currentUser;
+    try {
+      if (googleCurrentUser == null) googleCurrentUser = await _googleSignIn.signInSilently();
+      if (googleCurrentUser == null) googleCurrentUser = await _googleSignIn.signIn();
+      if (googleCurrentUser == null) return null;
+
+      GoogleSignInAuthentication googleAuth = await googleCurrentUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+      print("signed in " + user.displayName);
+
+      return user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  void transitionNextPage(FirebaseUser user) {
+    if (user == null) return;
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) =>
+        ChatPage(user)
+    ));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // メールアドレス入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'メールアドレス'),
+                onChanged: (String value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              // パスワード入力
+              TextFormField(
+                decoration: InputDecoration(labelText: 'パスワード'),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                // メッセージ表示
+                child: Text(infoText),
+              ),
+              Container(
+                width: double.infinity,
+                // ユーザー登録ボタン
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text('ユーザー登録'),
+                  onPressed: () async {
+                    try {
+                      // メール/パスワードでユーザー登録
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final AuthResult result = await auth.createUserWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                      final FirebaseUser user = result.user;
+                      // ユーザー登録に成功した場合
+                      // チャット画面に遷移＋ログイン画面を破棄
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          // ユーザー情報を渡す
+                          UserState().setUser(user);
+                          return ChatPage(user);
+                        }),
+                      );
+                    } catch (e) {
+                      /* --- 省略 --- */
+                    }
+                  },
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                // ログイン登録ボタン
+                child: OutlineButton(
+                  textColor: Colors.blue,
+                  child: Text('ログイン'),
+                  onPressed: () async {
+                    try {
+                      // メール/パスワードでログイン
+                      final FirebaseAuth auth = FirebaseAuth.instance;
+                      final AuthResult result = await auth.signInWithEmailAndPassword(
+                        email: email,
+                        password: password,
+                      );
+                      final FirebaseUser user = result.user;
+                      // ログインに成功した場合
+                      // チャット画面に遷移＋ログイン画面を破棄
+                      await Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) {
+                          // ユーザー情報を渡す
+                          UserState().setUser(user);
+                          return ChatPage(user);
+                        }),
+                      );
+                    } catch (e) {
+                      /* --- 省略 --- */
+                    }
+                  },
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                // ユーザー登録ボタン
+                child:RaisedButton(
+                  child: Text('Sign in Google'),
+                  onPressed: () {
+                    _handleSignIn()
+                        .then((FirebaseUser user) =>
+                        transitionNextPage(user)
+                    )
+                        .catchError((e) => print(e));
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleCurrentUser = _googleSignIn.currentUser;
+    try {
+      if (googleCurrentUser == null) googleCurrentUser = await _googleSignIn.signInSilently();
+      if (googleCurrentUser == null) googleCurrentUser = await _googleSignIn.signIn();
+      if (googleCurrentUser == null) return null;
+
+      GoogleSignInAuthentication googleAuth = await googleCurrentUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+      print("signed in " + user.displayName);
+
+      return user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  void transitionNextPage(FirebaseUser user) {
+    if (user == null) return;
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) =>
+        ChatPage(user)
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Sign in Google'),
+                onPressed: () {
+                  _handleSignIn()
+                      .then((FirebaseUser user) =>
+                      transitionNextPage(user)
+                  )
+                      .catchError((e) => print(e));
+                },
+              ),
+            ]
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_app/login.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_app/calender.dart';
+
+void main() {
+  // 最初に表示するWidget
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // 右上に表示される"debug"ラベルを消す
+      debugShowCheckedModeBanner: false,
+      // アプリ名
+      title: 'ChatApp',
+      theme: ThemeData(
+        // テーマカラー
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      // ログイン画面を表示
+      home: LoginPage(),
+      /*initialRoute: "/",
+        routes:<String, WidgetBuilder>{
+          "/login": (BuildContext context) => LoginPage(),
+          "/home": (BuildContext context) => ChatPage(UserState().user),
+        }*/
+    );
+  }
+}
+
+class UserState extends ChangeNotifier{
+  FirebaseUser user;
+  void setUser(FirebaseUser currentUser){
+    this.user = currentUser;
+    notifyListeners();
+  }
+}
+
+class LoginCheck extends StatefulWidget{
+  LoginCheck({Key key}) : super(key: key);
+
+  @override
+  _LoginCheckState createState() => _LoginCheckState();
+}
+
+class _LoginCheckState extends State<LoginCheck> {
+  //ログイン状態のチェック(非同期で行う)
+  void checkUser() async {
+    final currentUser = await FirebaseAuth.instance.currentUser();
+    final userState = Provider.of<UserState>(context, listen: false);
+    if (currentUser == null) {
+      @override
+      Widget build(BuildContext context) {
+        return LoginPage();
+      }
+    } else {
+      userState.setUser(currentUser);
+      @override
+      Widget build(BuildContext context) {
+        return ChatPage(currentUser);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkUser();
+  }
+  //ログイン状態のチェック時はこの画面が表示される
+  //チェック終了後にホーム or ログインページに遷移する
+  @override
+  Widget build(BuildContext context) {
+    checkUser();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text("Loading..."),
+        ),
+      ),
+    );
+  }
+}
+
+// チャット画面用Widget
+class ChatPage extends StatelessWidget {
+  // 引数からユーザー情報を受け取れるようにする
+  ChatPage(this.user);
+  // ユーザー情報
+  final FirebaseUser user;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('チャット'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () async {
+              // ログアウト処理
+              // 内部で保持しているログイン情報等が初期化される
+              // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
+              await FirebaseAuth.instance.signOut();
+              // ログイン画面に遷移＋チャット画面を破棄
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) {
+                  return LoginPage();
+                }),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text('ログイン情報：${user.email}'),
+          ),
+          Expanded(
+            // StreamBuilder
+            // 非同期処理の結果を元にWidgetを作れる
+            child: StreamBuilder<QuerySnapshot>(
+              // 投稿メッセージ一覧を取得（非同期処理）
+              // 投稿日時でソート
+              stream: Firestore.instance
+                  .collection('posts')
+                  .orderBy('date')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                // データが取得できた場合
+                if (snapshot.hasData) {
+                  final List<DocumentSnapshot> documents =
+                      snapshot.data.documents;
+                  // 取得した投稿メッセージ一覧を元にリスト表示
+                  return ListView(
+                    children: documents.map((document) {
+                      IconButton deleteIcon;
+                      // 自分の投稿メッセージの場合は削除ボタンを表示
+                      if (document['email'] == user.email) {
+                        deleteIcon = IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () async {
+                            // 投稿メッセージのドキュメントを削除
+                            await Firestore.instance
+                                .collection('posts')
+                                .document(document.documentID)
+                                .delete();
+                          },
+                        );
+                      }
+                      return Card(
+                        child: ListTile(
+                          title: Text(document['text']),
+                          subtitle: Text(document['email']),
+                          trailing: deleteIcon,
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+                // データが読込中の場合
+                return Center(
+                  child: Text('読込中...'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          // 投稿画面に遷移
+          await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 引数からユーザー情報を渡す
+              return AddPostPage(user);
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+// 投稿画面用Widget
+class AddPostPage extends StatefulWidget {
+  // ユーザー情報
+  final FirebaseUser user;
+  // 引数からユーザー情報を受け取る
+  AddPostPage(this.user);
+  @override
+  _AddPostPageState createState() => _AddPostPageState();
+}
+
+class _AddPostPageState extends State<AddPostPage> {
+  // 入力した投稿メッセージ
+  String messageText = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('チャット投稿'),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // 投稿メッセージ入力
+              TextFormField(
+                decoration: InputDecoration(labelText: '投稿メッセージ'),
+                // 複数行のテキスト入力
+                keyboardType: TextInputType.multiline,
+                // 最大3行
+                maxLines: 3,
+                onChanged: (String value) {
+                  setState(() {
+                    messageText = value;
+                  });
+                },
+              ),
+              Container(
+                width: double.infinity,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text('投稿'),
+                  onPressed: () async {
+                    final date =
+                    DateTime.now().toLocal().toIso8601String(); // 現在の日時
+                    final email = widget.user.email; // AddPostPage のデータを参照
+                    // 投稿メッセージ用ドキュメント作成
+                    await Firestore.instance
+                        .collection('posts') // コレクションID指定
+                        .document() // ドキュメントID自動生成
+                        .setData({
+                      'text': messageText,
+                      'email': email,
+                      'date': date
+                    });
+                    // 1つ前の画面に戻る
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+              FloatingActionButton(onPressed: (){
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) {
+                    // 引数からユーザー情報を渡す
+                    return CalenderExample();
+                  }),
+                );
+              },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
